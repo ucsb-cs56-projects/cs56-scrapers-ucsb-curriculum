ade36f43667688c2fd7ab1153008bdb543ccfbe7
Natasha <natashalee@umail.ucsb.edu>
2016-02-25 18:51:59 -0800

    added section tests and fixed section constructor

--- src/edu/ucsb/cs56/projects/scrapers/ucsb_curriculum/UCSBSectionTest.java
+++ src/edu/ucsb/cs56/projects/scrapers/ucsb_curriculum/UCSBSectionTest.java
@@ -12,12 +12,35 @@ import static org.junit.Assert.assertEquals;
 */
 
 public class UCSBSectionTest {
+	//Inititalize some variables to be used 
+	UCSBLecture parent = 
+	    new UCSBLecture("CMPSC 56", "Computer Science 56", "Full", "Conrad P",
+			    "M W", "9:00am - 9:50am", "HFH 1132", 50, 50);
 
+	String status = "Full";
+	String enrollCode = "07989"; 
+	String sectionDay = "T R";
+	String sectionTime = "3:30pm = 4:45pm";
+	String sectionRoom = "Chem 1171";
+	int enrolled = 63;
+	int capacity = 88;
 
     @Test
 	public void test1() {
-	// assertEquals(expected,actual);
+		UCSBSection test1 = new UCSBSection(parent, status, enrollCode, sectionDay,
+		 sectionTime, sectionRoom, enrolled, capacity);
+
+		//Test Constructor
+		assertEquals(parent, test1.getParent());
+		assertEquals(status, test1.getStatus());
+		assertEquals(enrollCode, test1.getEnrollCode());
+		assertEquals(sectionDay, test1.getSectionDay());
+		assertEquals(sectionTime,test1.getSectionTime());
+		assertEquals(sectionRoom, test1.getSectionRoom());
+		assertEquals(enrolled, test1.getEnrolled());
+		assertEquals(capacity, test1.getCapacity());
+
+		String expected = test1.toString(););
     }
     
-    // TODO: Write test cases
 }
