35c1d473366a6981c0b0843646296fab7ef24da8
Nathan Inge <ninge@umail.ucsb.edu>
2018-03-14 13:22:47 -0700

    Refactored UCSBLectureTest

--- src/edu/ucsb/cs56/projects/scrapers/ucsb_curriculum/UCSBLectureTest.java
+++ src/edu/ucsb/cs56/projects/scrapers/ucsb_curriculum/UCSBLectureTest.java
@@ -16,102 +16,142 @@ import org.junit.Test;
 public class UCSBLectureTest {
   
 //// Init some variables to be used
-//	String courseTitle = "CMPSC     8";
-//	String primaryCourseAbbr = "INTRO TO COMP SCI";
-//	String status = "Closed";
-//	String instructor = "BUONI M J";
-//	String lectDays = "T R";
-//	String lectTime = "3:30pm - 4:45pm";
-//	String lectRoom = "CHEM 1171";
-//	int enrolled = 63;
-//	int capacity = 88;
-//	String enrollCode = "00000";
-//
-//    /* Test the constructor written for issue 396
-//     TODO: Extend test when extended cstruct is written */
+	String courseTitle = "CMPSC     8";
+	String primaryCourseAbbr = "INTRO TO COMP SCI";
+	String status = "Closed";
+	String instructor = "BUONI M J";
+	String days = "T R";
+	String time = "3:30pm - 4:45pm";
+	String room = "CHEM 1171";
+	int enrolled = 63;
+	int capacity = 88;
+	String enrollCode = "00000";
+    String grading = "Letter";
+    String units= "4.0";
+    String college = "ENGR";
+    String preRequisite = "None";
+    String description = "See dept";
+    String fullTitle = "Introduction to CS";
+    String levelLimit = "None";
+    String majorLimitPass = "CSMPC";
+    String messages = "None";
+    String majorLimit = "None";
 	
      @Test
 	public void test_Constructor() {
-    	 // Placeholder until tests are refactored
-    	 assertTrue(true);
-
+		UCSBLecture lecture = 
+		    new UCSBLecture(
+		    		courseTitle,
+		    		primaryCourseAbbr,
+		    		status,
+		    		instructor,
+		    		days,   		
+				    time,
+				    room,
+				    grading,
+				    units,
+				    college,
+				    preRequisite,
+				    description,
+				    fullTitle,
+				    enrolled,
+				    capacity,
+				    enrollCode,
+				    levelLimit,
+				    majorLimitPass,
+				    messages,
+				    majorLimit);
 	
-
-//		UCSBLecture lecture = 
-//		    new UCSBLecture(courseTitle, primaryCourseAbbr, status, instructor,
-//				    lectDays, lectTime, lectRoom, enrolled, capacity, enrollCode);
-//	
-//		// Test Constructor
-//		assertEquals(courseTitle, lecture.getCourseTitle());
-//		assertEquals(primaryCourseAbbr, lecture.getPrimaryCourseAbbr());
-//		assertEquals(status, lecture.getStatus());
-//		assertEquals(instructor, lecture.getInstructor());
-//		assertEquals(lectDays, lecture.getDays());
-//		assertEquals(lectTime, lecture.getTime());
-//		assertEquals(lectRoom, lecture.getRoom());
-//		assertEquals(enrolled, lecture.getEnrolled());
-//		assertEquals(capacity, lecture.getCapacity());
-//		assertEquals(enrollCode, lecture.getEnrollCode());
-//	
-//		// Test Setters/Getters
-//		lecture.setCourseTitle("CMPSC 56");
-//		lecture.setPrimaryCourseAbbr("Computer Science 56");
-//		lecture.setStatus("Full");
-//		lecture.setInstructor("CONRAD P");
-//		lecture.setDays("M W");
-//		lecture.setTime("9:00am - 9:50am");
-//		lecture.setRoom("HFH 1132");
-//		lecture.setEnrolled(50);
-//		lecture.setCapacity(50);
-//		lecture.setEnrollCode("00000");
-//	
-//		assertEquals("CMPSC 56", lecture.getCourseTitle());
-//		assertEquals("Computer Science 56", lecture.getPrimaryCourseAbbr());
-//		assertEquals("Full", lecture.getStatus());
-//		assertEquals("CONRAD P", lecture.getInstructor());
-//		assertEquals("M W", lecture.getDays());
-//		assertEquals("9:00am - 9:50am", lecture.getTime());
-//		assertEquals("HFH 1132", lecture.getRoom());
-//		assertEquals(50, lecture.getEnrolled());
-//		assertEquals(50, lecture.getCapacity());
-//		assertEquals("00000", lecture.getEnrollCode());
+		// Test Constructor
+     	assertEquals(courseTitle, lecture.getCourseTitle());
+		assertEquals(primaryCourseAbbr, lecture.getPrimaryCourseAbbr());
+		assertEquals(status, lecture.getStatus());
+		assertEquals(instructor, lecture.getInstructor());
+		assertEquals(days, lecture.getDays());
+		assertEquals(time, lecture.getTime());
+		assertEquals(room, lecture.getRoom());
+		assertEquals(enrolled, lecture.getEnrolled());
+		assertEquals(capacity, lecture.getCapacity());
+		assertEquals(enrollCode, lecture.getEnrollCode());
+	
+		// Test Setters/Getters
+		lecture.setCourseTitle("CMPSC 56");
+		lecture.setPrimaryCourseAbbr("Computer Science 56");
+		lecture.setStatus("Full");
+		lecture.setInstructor("CONRAD P");
+		lecture.setDays("M W");
+		lecture.setTime("9:00am - 9:50am");
+		lecture.setRoom("HFH 1132");
+		lecture.setEnrolled(50);
+		lecture.setCapacity(50);
+		lecture.setEnrollCode("00000");
+	
+		assertEquals("CMPSC 56", lecture.getCourseTitle());
+		assertEquals("Computer Science 56", lecture.getPrimaryCourseAbbr());
+		assertEquals("Full", lecture.getStatus());
+		assertEquals("CONRAD P", lecture.getInstructor());
+		assertEquals("M W", lecture.getDays());
+		assertEquals("9:00am - 9:50am", lecture.getTime());
+		assertEquals("HFH 1132", lecture.getRoom());
+		assertEquals(50, lecture.getEnrolled());
+		assertEquals(50, lecture.getCapacity());
+		assertEquals("00000", lecture.getEnrollCode());
 
     }
 
-//    @Test
-//    public void test_toString(){
-//        UCSBLecture lecture =
-//	    new UCSBLecture(courseTitle, primaryCourseAbbr, status, instructor,
-//			    lectDays, lectTime, lectRoom, enrolled, capacity, enrollCode);
-//        
-//        String expected = "Course Title: " + courseTitle + "\n"
-//        + "Course Abbreviation: " + primaryCourseAbbr + "\n"
-//        
-//        + "\n ---- Course Details for " + courseTitle + "----\n"
-//        + "Course Full Title: " + "null" + "\n"
-//        + "Course Description: " + "null" + "\n"
-//        + "Course Prerequisites: " + "null" + "\n"
-//        + "College: " + "null" + "\n"
-//        + "Messages: " + "null" + "\n"
-//        
-//        + "\n ---- Enrollment Details for " + courseTitle + "----\n"
-//        + "Lecture Status: " + status + "\n"
-//        + "Enrolled / Capacity: " + enrolled + " / " + capacity + "\n"
-//        + "Units: " + "null" + "\n"
-//        + "Grading: " + "null" + "\n"
-//        + "Level Limit: " + "null" + "\n"
-//        + "Major Limit: " + "null" + "\n"
-//        + "Major Limit Pass: " + "null" + "\n"
-//        + "Enroll Code: " + enrollCode + "\n"
-//        
-//        + "\n ---- Lecture Details for " + courseTitle + "----\n"
-//        + "Lecture Instructor: " + instructor + "\n"
-//        + "Lecture Days: " + lectDays + "\n"
-//        + "Lecture Time: " + lectTime + "\n"
-//        + "Lecture Room: " + lectRoom + "\n";
-//
-//        assertEquals(expected, lecture.toString());
-//    }
+    @Test
+    public void test_toString(){
+    	UCSBLecture lecture = 
+    		    new UCSBLecture(
+    		    		courseTitle,
+    		    		primaryCourseAbbr,
+    		    		status,
+    		    		instructor,
+    		    		days,   		
+    				    time,
+    				    room,
+    				    grading,
+    				    units,
+    				    college,
+    				    preRequisite,
+    				    description,
+    				    fullTitle,
+    				    enrolled,
+    				    capacity,
+    				    enrollCode,
+    				    levelLimit,
+    				    majorLimitPass,
+    				    messages,
+    				    majorLimit);
+        
+        String expected = "Course Title: " + courseTitle + "\n"
+       + "Course Abbreviation: " + primaryCourseAbbr + "\n"
+        
+        + "\n ---- Course Details for " + courseTitle + "----\n"
+        + "Course Full Title: " + "Introduction to CS" + "\n"
+        + "Course Description: " + "See dept" + "\n"
+        + "Course Prerequisites: " + "None" + "\n"
+        + "College: " + "ENGR" + "\n"
+        + "Messages: " + "None" + "\n"
+        
+        + "\n ---- Enrollment Details for " + courseTitle + "----\n"
+        + "Lecture Status: " + status + "\n"
+        + "Enrolled / Capacity: " + enrolled + " / " + capacity + "\n"
+        + "Units: " + "4.0" + "\n"
+        + "Grading: " + "Letter" + "\n"
+        + "Level Limit: " + "None" + "\n"
+        + "Major Limit: " + "None" + "\n"
+        + "Major Limit Pass: " + "CSMPC" + "\n"
+        + "Enroll Code: " + enrollCode + "\n"
+        
+        + "\n ---- Lecture Details for " + courseTitle + "----\n"
+        + "Lecture Instructor: " + instructor + "\n"
+        + "Lecture Days: " + days + "\n"
+        + "Lecture Time: " + time + "\n"
+        + "Lecture Room: " + room + "\n";
+
+        assertEquals(expected, lecture.toString());
+    }
     
 
 }
--- src/edu/ucsb/cs56/projects/scrapers/ucsb_curriculum/UCSBSectionTest.java
+++ src/edu/ucsb/cs56/projects/scrapers/ucsb_curriculum/UCSBSectionTest.java
@@ -17,12 +17,6 @@ public class UCSBSectionTest {
 	
     @Test
 	public void test1() {
-    	
-    	
-    	// Placeholder until tests are refactored
-   	 	assertTrue(false);
-   	 
-   	 
 		//initialize section parent
 		UCSBLecture parent =
 		new UCSBLecture("CMPSC 56", "Computer Science 56", "Full", "Conrad P",
@@ -65,103 +59,5 @@ public class UCSBSectionTest {
 		
 
     }
-	
-//	@Test
-//	public void test2() {
-//		//initialize section parent
-//		UCSBLecture parent =
-//		new UCSBLecture("CMPSC 64", "Computer Science 64", "", "Dewey K",
-//						"T R", "3:30pm - 4:45pm", "Phelps 3526", 5, 80, "00000");
-//		
-//		//variables used for section testing
-//		String status = "";
-//		String enrollCode = "36555";
-//		String sectionDay = "T";
-//		String sectionTime = "5:30pm - 6:30pm";
-//		String sectionRoom = "Phelps 3526";
-//		int enrolled = 5;
-//		int capacity = 80;
-//		
-//		//Test Constructor
-//		UCSBSection test2 = new UCSBSection(parent, status, enrollCode, sectionDay,
-//											sectionTime, sectionRoom, enrolled, capacity);
-//		
-//		
-//		//test getter methods
-//		assertEquals(parent, test2.getParent());
-//		assertEquals(status, test2.getStatus());
-//		assertEquals(enrollCode, test2.getEnrollCode());
-//		assertEquals(sectionDay, test2.getSectionDay());
-//		assertEquals(sectionTime,test2.getSectionTime());
-//		assertEquals(sectionRoom, test2.getSectionRoom());
-//		assertEquals(enrolled, test2.getEnrolled());
-//		assertEquals(capacity, test2.getCapacity());
-//		
-//		//test tostring method
-//		String expected = "\t Course Title: CMPSC 64\n"
-//		+ "\t Section Status: \n"
-//		+ "\t Enroll Code: 36555\n"
-//		+ "\t Section Day: T\n"
-//		+ "\t Section Time: 5:30pm - 6:30pm\n"
-//		+ "\t Section Rm: Phelps 3526\n"
-//		+ "\t Enrolled / Capacity: 5 / 80\n";
-//		assertEquals(expected, test2.toString());
-//		
-//	}
-	
-//	
-//	@Test
-//	public void test3() {
-//		//initialize section parent
-//		UCSBLecture parent =
-//		new UCSBLecture("ECON 10A", "Microecon Theory", "", "Hartman J L",
-//						"M W F", "12:00pm - 12:50pm", "Buchn 1910", 194, 200, "");
-//
-//		//variables used for section testing
-//		String status = "Closed";
-//		String enrollCode = "12815";
-//		String sectionDay = "R";
-//		String sectionTime = "5:00pm - 5:50pm";
-//		String sectionRoom = "Girv 2119";
-//		int enrolled = 0;
-//		int capacity = 40;
-//		
-//		//Test Constructor
-//		UCSBSection test3 = new UCSBSection(parent, status, enrollCode, sectionDay,
-//											sectionTime, sectionRoom, enrolled, capacity);
-//		
-//		//test setter methods
-//		test3.setParent(parent);
-//		test3.setStatus(status);
-//		test3.setEnrollCode(enrollCode);
-//		test3.setSectionDay(sectionDay);
-//		test3.setSectionTime(sectionTime);
-//		test3.setSectionRoom(sectionRoom);
-//		test3.setEnrolled(enrolled);
-//		test3.setCapacity(capacity);
-//
-//		//test getter methods
-//		assertEquals(parent, test3.getParent());
-//		assertEquals(status, test3.getStatus());
-//		assertEquals(enrollCode, test3.getEnrollCode());
-//		assertEquals(sectionDay, test3.getSectionDay());
-//		assertEquals(sectionTime,test3.getSectionTime());
-//		assertEquals(sectionRoom, test3.getSectionRoom());
-//		assertEquals(enrolled, test3.getEnrolled());
-//		assertEquals(capacity, test3.getCapacity());
-//		
-//		//test tostring method
-//		String expected = "\t Course Title: ECON 10A\n"
-//		+ "\t Section Status: Closed\n"
-//		+ "\t Enroll Code: 12815\n"
-//		+ "\t Section Day: R\n"
-//		+ "\t Section Time: 5:00pm - 5:50pm\n"
-//		+ "\t Section Rm: Girv 2119\n"
-//		+ "\t Enrolled / Capacity: 0 / 40\n";
-//		assertEquals(expected, test3.toString());
-//
-//
-//		
-//	}
 	
 }
